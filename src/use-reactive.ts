7fb3e2e2bc17ecdbd52f41bc5cd1d9d59f7c66b7
yunliang <804408086@qq.com>
2024-03-29 19:23:35 +0800

    fixed

--- src/use-reactive.ts
+++ src/use-reactive.ts
@@ -32,29 +32,23 @@ const useCreation = <T>(factory: () => T, deps: DependencyList) => {
   }
   return current.obj as T;
 };
-
 // k:v 原对象:代理过的对象
 const proxyMap = new WeakMap();
 // k:v 代理过的对象:原对象
 const rawMap = new WeakMap();
-
 function observer<T extends Record<string, any>>(
   initialVal: T,
   cb: () => void
 ): T {
   const existingProxy = proxyMap.get(initialVal);
-
   // 添加缓存 防止重新构建proxy
   if (existingProxy) {
     return existingProxy;
   }
-
   // 防止代理已经代理过的对象
-  // https://github.com/alibaba/hooks/issues/839
   if (rawMap.has(initialVal)) {
     return initialVal;
   }
-
   const proxy = new Proxy<T>(initialVal, {
     get(target, key, receiver) {
       const res = Reflect.get(target, key, receiver);
@@ -75,14 +69,12 @@ function observer<T extends Record<string, any>>(
       return ret;
     },
   });
-
   proxyMap.set(initialVal, proxy);
   rawMap.set(proxy, initialVal);
-
   return proxy;
 }
 
-export default <S extends Record<string, any>>(initialState: S): S  => {
+export default <S extends Record<string, any>>(initialState: S): S => {
   const refresh = useRefresh();
   const stateRef = useRef<S>(initialState);
   const state = useCreation(() => {
@@ -90,6 +82,5 @@ export default <S extends Record<string, any>>(initialState: S): S  => {
       refresh();
     });
   }, []);
-
   return state;
-}
+};
